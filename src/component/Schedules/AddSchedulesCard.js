c240c10a2a577ebda488f6d66ec2e9ca620df8b5
madleenshanwar <shanwarmido@gmail.com>
2024-10-21 11:27:28 +0300

    I have solved some problems in AddScheduleCard componrent and i have changed the way values are displayed in the select field

--- src/component/Schedules/AddSchedulesCard.js
+++ src/component/Schedules/AddSchedulesCard.js
@@ -1,29 +1,37 @@
-import { Box, Button, Chip, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Chip,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 const validationSchema = Yup.object().shape({
-  shift_type: Yup.string()
-    .required("shift_type is required"),
-    date: Yup.string()
-    .required("date is required"),
-    start_time: Yup.string()
-    .required("start_time is required"),
-    end_time: Yup.string()
-    .required("end_time is required"),
-    doctor_id:Yup.array()
-    .min(1,"you must select at least one option")
-    .of(Yup.string().required('option is required'))
-  })
+  shift_type: Yup.string().required("shift_type is required"),
+  date: Yup.string().required("date is required"),
+  start_time: Yup.string().required("start_time is required"),
+  end_time: Yup.string().required("end_time is required"),
+  doctor_id: Yup.array()
+    .min(1, "you must select at least one option")
+    .of(Yup.string().required("option is required")),
+});
 export default function AddSchedulesCard() {
-  const [schedule,setSchedule]=useState({
-    shift_type:'',
-    date:'',
-    start_time:"",
-    end_time:"",
-    doctor_id:[]
-  })
-  const Doctors=[{id:'1',name:'george'},{id:'2',name:'saad'}]
+  const [schedule, setSchedule] = useState({
+    shift_type: "",
+    date: "",
+    start_time: "",
+    end_time: "",
+    doctor_id: [],
+  });
+  const Doctors = [
+    { id: "1", name: "george" },
+    { id: "2", name: "saad" },
+  ];
   const route = useNavigate();
   const [errors, setErrors] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -33,7 +41,7 @@ export default function AddSchedulesCard() {
       ...prevD,
       [name]: value,
     }));
-    console.log(schedule)
+    console.log(schedule);
     try {
       await validationSchema.validateAt(name, { [name]: value });
       setErrors((prev) => ({ ...prev, [name]: undefined }));
@@ -65,72 +73,72 @@ export default function AddSchedulesCard() {
   }, [isSubmitted, route]);
   return (
     <Box
-    component="form"
-    onSubmit={handleSubmit}
-    sx={{
-      margin: "70px auto",
-      p: 3,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      gap: 2,
-      boxShadow: "0px 4px 10px rgba(0,0,0,0.25)",
-      borderRadius: 8,
-      backgroundColor: "rgba(255,255,255,0.7)",
-      border: "1px solid #00ACB1",
-      height: "fit-content",
-      maxWidth: "500px",
-    }}
-  >
-     <TextField
-          id="outlined-select-currency"
-          select
-          label="please select your shift_type"
-          onChange={handleChange}
-          value={schedule.shift_type}
-          name="shift_type"
-          error={Boolean(errors.shift_type)}
-          helperText={errors.shift_type}
-          fullWidth
-        >
-          <MenuItem value="day">Day</MenuItem>
-          <MenuItem value="night">Night</MenuItem>
-          <MenuItem value="evening">Evining</MenuItem>
-        </TextField>
-        <TextField
-          type='date'
-          name="date"
-          variant="outlined"
-          value={schedule.date}
-          onChange={handleChange}
-          error={Boolean(errors.date)}
-          helperText={errors.date}
-          fullWidth
-        />
-        <TextField
-          placeholder="Start Time"
-          type='time'
-          name="start_time"
-          variant="outlined"
-          value={schedule.start_time}
-          onChange={handleChange}
-          error={Boolean(errors.start_time)}
-          helperText={errors.start_time}
-          fullWidth
-        />
-        <TextField
-          placeholder="End Time"
-          type='time'
-          name="end_time"
-          variant="outlined"
-          value={schedule.end_time}
-          onChange={handleChange}
-          error={Boolean(errors.end_time)}
-          helperText={errors.end_time}
-          fullWidth
-        />
-        <FormControl fullWidth>
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        margin: "70px auto",
+        p: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 2,
+        boxShadow: "0px 4px 10px rgba(0,0,0,0.25)",
+        borderRadius: 8,
+        backgroundColor: "rgba(255,255,255,0.7)",
+        border: "1px solid #00ACB1",
+        height: "fit-content",
+        maxWidth: "500px",
+      }}
+    >
+      <TextField
+        id="outlined-select-currency"
+        select
+        label="please select your shift_type"
+        onChange={handleChange}
+        value={schedule.shift_type}
+        name="shift_type"
+        error={Boolean(errors.shift_type)}
+        helperText={errors.shift_type}
+        fullWidth
+      >
+        <MenuItem value="day">Day</MenuItem>
+        <MenuItem value="night">Night</MenuItem>
+        <MenuItem value="evening">Evining</MenuItem>
+      </TextField>
+      <TextField
+        type="date"
+        name="date"
+        variant="outlined"
+        value={schedule.date}
+        onChange={handleChange}
+        error={Boolean(errors.date)}
+        helperText={errors.date}
+        fullWidth
+      />
+      <TextField
+        placeholder="Start Time"
+        type="time"
+        name="start_time"
+        variant="outlined"
+        value={schedule.start_time}
+        onChange={handleChange}
+        error={Boolean(errors.start_time)}
+        helperText={errors.start_time}
+        fullWidth
+      />
+      <TextField
+        placeholder="End Time"
+        type="time"
+        name="end_time"
+        variant="outlined"
+        value={schedule.end_time}
+        onChange={handleChange}
+        error={Boolean(errors.end_time)}
+        helperText={errors.end_time}
+        fullWidth
+      />
+      <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label"> doctors team</InputLabel>
         <Select
           id="demo-simple-select-label"
@@ -142,17 +150,22 @@ export default function AddSchedulesCard() {
           error={Boolean(errors.doctor_id)}
           helperText={errors.doctor_id}
           renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip key={value} label={value} />
-              ))}
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              {selected.map((value) => {
+                const doctor = Doctors.find((doc) => doc.id === value);
+                return <Chip key={value} label={doctor ? doctor.name : ""} />; 
+              })}
             </Box>
           )}
         >
-          {Doctors.map(doctor=><MenuItem key={doctor.id} value={doctor.name}>{doctor.name}</MenuItem>)}
+          {Doctors.map((doctor) => (
+            <MenuItem key={doctor.id} value={doctor.id}>
+              {doctor.name}
+            </MenuItem>
+          ))}
         </Select>
-        </FormControl>
-        <Box sx={{ display: "flex", gap: 2 }}>
+      </FormControl>
+      <Box sx={{ display: "flex", gap: 2 }}>
         <Button
           variant="contained"
           type="submit"
@@ -176,5 +189,5 @@ export default function AddSchedulesCard() {
         </Button>
       </Box>
     </Box>
-  )
+  );
 }
